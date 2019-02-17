a20ab6e74a12a773625f5e3399ce0052e20c92c6
Mailon Torres <mr4torr@gmail.com>
2019-02-17 16:04:53 -0300

    Update dashboard_controller.rb

--- app/controllers/painel/dashboard_controller.rb
+++ app/controllers/painel/dashboard_controller.rb
@@ -1,6 +1,14 @@
 class Painel::DashboardController < Painel::PainelController
 
+    before_action :get_tweets
+
     def index
+        @tags = Tag.all if !request.xhr?
     end
 
+    protected
+
+    def get_tweets
+        @tweets = Tweet.get_by_hashtag.page(params[:page])
+    end
 end
